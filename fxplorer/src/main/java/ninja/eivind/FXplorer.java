47b4df866572364da1a0fed58cb6b3900b692f89
eivindveg <eivindveg@hotmail.com>
2015-08-19 22:56:01 +0200

    add mockup internet browser

---
+++ fxplorer/src/main/java/ninja/eivind/FXplorer.java
@@ -0,0 +1,58 @@
+package ninja.eivind;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class FXplorer extends Application {
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private WebView web;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     * <p/>
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        URL resource = FXplorer.class.getResource("FXplorer.fxml");
+        assert resource != null;
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(resource);
+        Parent root = fxmlLoader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @FXML
+    public void update() {
+        String urlString = input.getText();
+        web.getEngine().load(urlString);
+    }
+}
