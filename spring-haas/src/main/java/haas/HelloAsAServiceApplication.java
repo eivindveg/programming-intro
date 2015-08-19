b6c7db623bc7ffb709e1441ca42c0df674e3a3d3
eivindveg <eivindveg@hotmail.com>
2015-08-19 17:00:17 +0200

    Add spring-haas stub

---
+++ spring-haas/src/main/java/haas/HelloAsAServiceApplication.java
@@ -0,0 +1,12 @@
+package haas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAsAServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAsAServiceApplication.class, args);
+    }
+}
---
+++ spring-haas/src/test/java/haas/HelloAsAServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package haas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloAsAServiceApplication.class)
+@WebAppConfiguration
+public class HelloAsAServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
